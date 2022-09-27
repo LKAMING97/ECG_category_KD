219fff09036259e2d1ed19757e2af5ad1bf03798
LKAMING97 <564255623@qq.com>
2022-09-27 16:58:17 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:lkaming
+@File:config.py
+@Time:2022/9/27 16:01
+
+"""
---
+++ train.py
@@ -0,0 +1,17 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author:lkaming
+@File:train.py
+@Time:2022/9/27 15:49
+
+"""
+import numpy as np
+
+if __name__ == "__main__":
+
+    
+    label = np.load("data/label.npy")
+    data = np.load("data/data.npy")
+    data1 = np.load("data/age_gender.npy")
+    pass
